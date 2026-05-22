a5c3d2145ff875aef27c8cee961fe29bfdb00bd1
shahidofical01 <malikshahidoffical01@gmail.com>
2026-05-22 19:01:18 +0500

    First commit: Adding my python code

---
+++ 1.py
@@ -0,0 +1,10 @@
+text="This is the main data from where i tyoe this fro nothing do to us to do this fro the resaon of what we are doing"
+
+chunks=[]
+
+chunk_size= 35
+
+for i in range(0, len(text), chunk_size):
+    chunk= text[i:i + chunk_size]
+    chunks.append(chunk)
+print(chunks)
