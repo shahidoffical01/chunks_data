697c10be73f8b0b8f271347ea47b0706ebcef2c4
shahidofical01 <malikshahidoffical01@gmail.com>
2026-05-23 05:55:09 +0500

    first commit

---
+++ 2.py
@@ -0,0 +1,10 @@
+text="The website is warning you:We will not display it again. This means once you close this browser tab, you cannot come back to copy this exact key."
+
+chunks=[]
+
+chunk_size= 20
+
+for i in range(0, len(text), chunk_size):
+    chunk= text[i:i + chunk_size]
+    chunks.append(chunk)
+print(chunks)
