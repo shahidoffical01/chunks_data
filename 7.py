a2727cef79da9082c7a86c84f251156d2f913f27
shahidofical01 <malikshahidoffical01@gmail.com>
2026-07-22 21:37:26 +0500

    k

--- 7.py
+++ 7.py
@@ -2,7 +2,7 @@ from chunking_tools import chunk_string, chunk_generator
 
 # Chunking Strings
 text = "Supercalifragilisticexpialidocious"
-print(chunk_string(text, 13))
+print(chunk_string(text, 12))
 # Output: ['Supercalif', 'ragilistic', 'expialidoc', 'ious']
 
 # Chunking standard iterables without padding
